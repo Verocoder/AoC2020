9f44fd78a4d2b042cf47ae64d6053e1d7ea8e9cc
verocoder <david@verophotography.co.uk>
2020-12-01 22:12:04 +0000

    upgraded to replace hand tests with doctests

--- Day01/Day01_2.py
+++ Day01/Day01_2.py
@@ -1,4 +1,12 @@
+
 def solveArray(sample_data):
+    """Return the factorial of n, an exact integer >= 0.
+
+    >>> solveArray([979, 366, 675, 123, 456])
+    241861950
+    >>> solveArray([366, 675, 123, 456])
+
+    """
     for x in sample_data:
         x = int(x)
         for y in sample_data:
@@ -9,25 +17,15 @@ def solveArray(sample_data):
                 if sum == 2020:
                     answer = x * y * z
                     return answer
+    return None
 
-def test1():
-    answer = solveArray([979, 366, 675,123, 456])
-    if answer == 241861950:
-        print("test 1 pass")
-    else:
-        print("test 1 fail")
 
-def test2():
-    answer = solveArray([366, 675,123, 456])
-    if answer == 241861950:
-        print("test 2 fail")
-    else:
-        print("test 2 pass")
 
-test1()
-test2()
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
-file1 = open('sampleData', 'r')
-sample_data = file1.readlines()
-print("size of sample_data list: " + str(len(sample_data)))
-print("the answer is: " + str(solveArray(sample_data)))
+    file1 = open('sampleData', 'r')
+    sample_data = file1.readlines()
+    print("size of sample_data list: " + str(len(sample_data)))
+    print("the answer is: " + str(solveArray(sample_data)))
