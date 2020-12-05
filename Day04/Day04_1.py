82c344ac2a620c297c98ab2386c3481a9038ae39
verocoder <david@verophotography.co.uk>
2020-12-05 18:34:34 +0000

    Day4 part 1, star first run with data!

--- Day04/Day04_1.py
+++ Day04/Day04_1.py
@@ -8,7 +8,7 @@ def isValid(passport):
     False
     >>> isValid('hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm')
     True
-    >>> isValid('hcl:#cfa07d eyr:2025 pid:166559648iyr:2011 ecl:brn hgt:59in')
+    >>> isValid('hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in')
     False
     """
     keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
@@ -32,6 +32,45 @@ def makeMapFromString(passportString):
         passportMap[key] = value
     return passportMap
 
+def parseArray(arrayOfLines):
+    """
+    :param arrayOfLines: ungrouped
+    :return: array of passport lists
+    >>> len(parseArray(getTestArray()))
+    4
+    >>> parseArray(getTestArray())[0]
+    'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm'
+    >>> parseArray(getTestArray())[1]
+    'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929'
+    >>> parseArray(getTestArray())[2]
+    'hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm'
+    >>> parseArray(getTestArray())[3]
+    'hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in'
+    """
+    arrayOfPassports = []
+    lastPassportString = ""
+    for line in arrayOfLines:
+        if line == "\n":
+            if lastPassportString != "":
+                arrayOfPassports.append(lastPassportString.strip(' '))
+                lastPassportString = ""
+        else:
+            lastPassportString += line.strip('\n')
+            lastPassportString += ' '
+    arrayOfPassports.append(lastPassportString.strip(' '))
+    return arrayOfPassports
+
+def run(sampleData):
+    passportArray = parseArray(sampleData)
+    counter = 0
+    for passport in passportArray:
+        if isValid(passport):
+            counter += 1
+    return counter
+
+def getTestArray():
+    file = open('testData', 'r')
+    return file.readlines()
 
 if __name__ == "__main__":
     import doctest
@@ -41,4 +80,4 @@ if __name__ == "__main__":
     sample_data = file.readlines()
     print("size of sample_data list: " + str(len(sample_data)))
     # TODO needs a dunction to take white space separated arbitrary multiline info and turn into an array of strings
-    # print("the answer is: " + str(run(sample_data)))
+    print("the answer is: " + str(run(sample_data)))
