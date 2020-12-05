2727a2343dab3a6f022b50645d5d8973ce384bba
verocoder <david@verophotography.co.uk>
2020-12-05 19:23:38 +0000

    Day4 part 2 updated test data, fixed more precise regex matching. Now correct

--- Day04/Day04_2.py
+++ Day04/Day04_2.py
@@ -2,14 +2,22 @@ def isValid(passportString):
     """
     :param passport: string of a passport
     :return:boolean of whether valid
-    >>> isValid('ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm')
-    True
-    >>> isValid('iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929')
+    >>> isValid('eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926')
     False
-    >>> isValid('hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm')
-    True
-    >>> isValid('hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in')
+    >>> isValid('iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946')
     False
+    >>> isValid('hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277')
+    False
+    >>> isValid('hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007')
+    False
+    >>> isValid('pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f')
+    True
+    >>> isValid('eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm')
+    True
+    >>> isValid('hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022')
+    True
+    >>> isValid('iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719')
+    True
     """
     import re
     passport = makeMapFromString(passportString)
@@ -49,7 +57,7 @@ def isValid(passportString):
     hairColourPattern = '#[0-9a-f]{6}'
     hairColourRegex = re.compile(hairColourPattern)
     if 'hcl' in passport.keys():
-        if not hairColourRegex.search(passport['hcl']):
+        if not hairColourRegex.fullmatch(passport['hcl']):
             return False
     else:
         return False
@@ -57,7 +65,7 @@ def isValid(passportString):
     eyeColourPattern = 'amb|blu|brn|gry|grn|hzl|oth'
     eyeColourRegex = re.compile(eyeColourPattern)
     if 'ecl' in passport.keys():
-        if not eyeColourRegex.search(passport['ecl']):
+        if not eyeColourRegex.fullmatch(passport['ecl']):
             return False
     else:
         return False
@@ -65,7 +73,7 @@ def isValid(passportString):
     idPattern = '[0-9]{9}'
     idRegex = re.compile(idPattern)
     if 'pid' in passport.keys():
-        if not idRegex.search(passport['pid']):
+        if not idRegex.fullmatch(passport['pid']):
             return False
     else:
         return False
