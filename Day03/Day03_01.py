010b9678e05def3721cfdfd11fdb93664a183cdc
verocoder <david@verophotography.co.uk>
2020-12-05 19:38:34 +0000

    Fixed Day03 part1 !!

--- Day03/Day03_01.py
+++ Day03/Day03_01.py
@@ -8,40 +8,22 @@ def runSubtraction(data):
     """
     countTrees = 0
     countX = 0
+    countY = 0
     for line in data:
-        lineList = list(line)
-        lineLen = len(lineList) - 1
+        lineList = list(line.strip('\n'))
+        lineLen = len(lineList)
         # check to see if gone off side of map and move back one map width if so
         if(countX >= lineLen):
             countX -= lineLen
+
+        # print(str(countX) + ',' + str(countY))
         if(line[countX]) == "#":
             countTrees += 1
         countX += 3
+        countY +=1
 
     return countTrees
 
-
-def runDivision(data):
-    """
-    :param data: tree map,1 uniqueness wide
-    :return: count of trees hit
-    >>> runDivision(getTestData())
-    7
-    """
-    countTrees = 0
-    countX = 0
-    countY = 0
-    for line in data:
-        lineLen = len(line)
-        # ??????
-        # check to see if gone off side of map and move back one map width if so
-        if(countX > lineLen):
-            countX -= lineLen
-        if(line[countX]) == "#":
-            countTrees += 1
-        countX += 3
-
-    return countTrees
 
 def getTestData():
     file = open('testData', 'r')
@@ -54,4 +36,4 @@ if __name__ == "__main__":
     file = open('sampleData', 'r')
     sample_data = file.readlines()
     print("size of sample_data list: " + str(len(sample_data)))
-    print("the answer is: " + str(run(sample_data)))
+    print("the answer is: " + str(runSubtraction(sample_data)))
